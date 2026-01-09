c448b4f0448476bf7b08dd336e4c536319729b4a
Ranuga Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2026-01-09 14:20:37 +0530

    feat: final implementation touches

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-import { z } from '@hono/zod-openapi';
-
 export interface Environment {
   DB: D1Database;
   R2_BUCKET: R2Bucket;
@@ -7,16 +5,3 @@ export interface Environment {
   BETTER_AUTH_URL: string;
   BETTER_AUTH_SECRET: string;
 }
-
-export const HelloWorldSchema = z
-  .object({
-    text: z.string(),
-  })
-  .openapi('HelloWorld');
-
-export const StatusSchema = z
-  .object({
-    status: z.string(),
-    service: z.string(),
-  })
-  .openapi('Status');
