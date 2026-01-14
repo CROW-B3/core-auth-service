0e29fcd549ca9a9f5cbbade67916d75645090eaf
Ranuga Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2026-01-15 01:07:38 +0530

    feat: initialize auth service (#7)

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const PROD_ORIGINS = [
+  'https://crowai.dev',
+  'https://app.crowai.dev',
+  'https://api.crowai.dev',
+  'https://dev.crowai.dev',
+  'https://dev.app.crowai.dev',
+  'https://dev.api.crowai.dev',
+];
+
+export const LOCAL_ORIGINS = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002',
+  'http://localhost:8000',
+];
